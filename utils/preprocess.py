2e16bba828cdccb599e482be632579b7807d8b73
EMobilio <eymobilio@gmail.com>
2025-03-28 13:26:15 -0400

    updated preprocess util for linear regression hadnling

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,11 +1,13 @@
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import numpy as np
 
 
-def preprocess(game_data):
+def preprocess(game_data, model):
     """
+        Takes a DataFrame containing game data and a string and performs encoding and scaling of the data,
+        returning the processed DataFrame.
     """
     # label encode precip and sky
     for col in ["precip", "sky"]:
@@ -18,6 +20,11 @@ def preprocess(game_data):
     # drop unnecessary columns
     game_data.drop(columns=["day_of_week_name", "date"], inplace=True)
 
+    # cyclic encoding for 'ay_of_week (if using linear model)
+    if model == "linear":
+        game_data["day_of_week_sin"] = np.sin(2 * np.pi * game_data["day_of_week"] / 7)
+        game_data["day_of_week_cos"] = np.cos(2 * np.pi * game_data["day_of_week"] / 7)
+
     # scale numerical features
     scaler = StandardScaler()
     num_cols = ["division_rank", "games_behind", "cLI", "streak", "runs_scored_pg", "runs_allowed_pg",
@@ -26,5 +33,3 @@ def preprocess(game_data):
     game_data[num_cols] = scaler.fit_transform(game_data[num_cols])
 
     return game_data
-
-    
